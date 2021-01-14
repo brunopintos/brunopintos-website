3568d16e737014bcdcb096cd9ac616b380f3429c
Bruno Pintos <brunopintos98@gmail.com>
2021-01-14 01:25:50 -0300

    added social media

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
-import Head from 'next/head'
+import Head from "next/head";
 import styled from "styled-components";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,19 +11,40 @@ export default function Home() {
       </Head>
 
       <MainContainer>
-        <h1>
-          Bruno Pintos
-        </h1>
-        <Subtitle style="#">
-          Software developer
-        </Subtitle>
-        <Image src="/bruno.png" alt="profile image"></Image>
+        <h1>Bruno Pintos</h1>
+        <Subtitle>Software developer</Subtitle>
+        <Image src="/bruno.png" alt="profile image" />
+        <SocialMediaImageContainer>
+          <a href="https://www.linkedin.com/in/bruno-pintos-sugliano-5bba04154/">
+            <SocialMediaImage src="/linkedin.png" alt="linkedin image" />
+          </a>
+          <a href="https://github.com/brunopintos">
+            <SocialMediaImage src="/github.png" alt="github image" />
+          </a>
+          <a href="https://twitter.com/brunopintos98">
+            <SocialMediaImage src="/twitter.png" alt="twitter image" />
+          </a>
+          <a href="mailto:brunopintos98@gmail.com">
+            <MailImageContainer>
+              <SocialMediaImage
+                style={{
+                  width: 28,
+                  borderRadius: 0,
+                }}
+                src="/mail.png"
+                alt="mail image"
+              />
+            </MailImageContainer>
+          </a>
+        </SocialMediaImageContainer>
       </MainContainer>
     </div>
-  )
+  );
 }
 
 const Subtitle = styled.h2`
+  margin-top: 0px;
+  margin-bottom: 40px;
   color: #3f51b5;
 `;
 
@@ -38,6 +60,30 @@ const MainContainer = styled.div`
 const Image = styled.img`
   max-width: 200px;
   border-radius: 50%;
-  -webkit-clip-path: circle(60.0% at 50% 10%);
-  clip-path: circle(50.0% at 50% 50%);
+`;
+
+const SocialMediaImageContainer = styled.div`
+  width: 200px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  margin-top: 20px;
+`;
+
+const SocialMediaImage = styled.img`
+  max-width: 40px;
+  border-radius: 50%;
+  cursor: pointer;
+`;
+
+const MailImageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+  background-color: black;
+  border-radius: 50%;
+  cursor: pointer;
 `;
